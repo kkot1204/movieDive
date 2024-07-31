206ece9c5818ac0774aeee7ec3d42e5abfc64acf
wldud7788 <rooftop7788@naver.com>
2024-07-31 18:40:59 +0900

    add: firebase sdk 설정

---
+++ js/login.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCmulI1n7YvyAtOtQumYNa5IaaPvEddBvQ",
+  authDomain: "movieaccount-3d409.firebaseapp.com",
+  projectId: "movieaccount-3d409",
+  storageBucket: "movieaccount-3d409.appspot.com",
+  messagingSenderId: "942641611452",
+  appId: "1:942641611452:web:864f3cd1c0918cfe14c6b9",
+  measurementId: "G-E97WVRRBKZ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
